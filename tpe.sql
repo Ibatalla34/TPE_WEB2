f0095e8c3153092950f28416f27a22524dafc879
ivan <ibatalla13@gmail.com>
2023-09-26 23:07:20 -0300

    editado archivos readme,etc

--- tpe.sql
+++ tpe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-09-2023 a las 01:34:49
+-- Tiempo de generación: 27-09-2023 a las 03:57:06
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `clubes` (
-  `Id_equipos` int(11) NOT NULL,
-  `Nombre` varchar(100) NOT NULL,
+  `Id_Clubes` int(11) NOT NULL,
+  `Equipo` varchar(100) NOT NULL,
   `Liga` varchar(100) NOT NULL,
   `Ciudad` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -38,14 +38,14 @@ CREATE TABLE `clubes` (
 -- Volcado de datos para la tabla `clubes`
 --
 
-INSERT INTO `clubes` (`Id_equipos`, `Nombre`, `Liga`, `Ciudad`) VALUES
+INSERT INTO `clubes` (`Id_Clubes`, `Equipo`, `Liga`, `Ciudad`) VALUES
 (1, 'Inter Miami', 'Major League Soccer', 'Miami'),
-(2, 'Paris Saint-Germain', 'Ligue 1', 'Paris'),
-(3, 'Manchester City', 'Premier League', 'Manchester'),
-(4, 'Real Madrid', 'Liga Española', 'Madrid'),
-(5, 'Atlético de Madrid', 'Liga Española', 'Madrid'),
-(6, 'Barcelona', 'Liga Española', 'Barcelona'),
-(7, 'Liverpool', 'Premier League', 'Liverpool');
+(2, 'Paris Saint-Germain  ', 'Ligue 1', 'Paris'),
+(3, 'Manchester City ', 'Premier League', 'Manchester'),
+(4, 'Real Madrid  ', 'Liga Española', 'Madrid'),
+(5, 'Atlético de Madrid ', 'Liga Española', 'Madrid'),
+(6, 'Barcelona ', 'Liga Española', 'Barcelona'),
+(7, 'Liverpool ', 'Premier League', 'Liverpool');
 
 -- --------------------------------------------------------
 
@@ -54,30 +54,30 @@ INSERT INTO `clubes` (`Id_equipos`, `Nombre`, `Liga`, `Ciudad`) VALUES
 --
 
 CREATE TABLE `futbolistas` (
-  `Id_jugador` int(11) NOT NULL,
+  `Id_Futbolistas` int(11) NOT NULL,
   `Nombre` varchar(100) NOT NULL,
   `Apellido` varchar(100) NOT NULL,
   `Edad` int(100) NOT NULL,
   `Pais` varchar(100) NOT NULL,
-  `Id_equipos` int(11) NOT NULL
+  `Id_Clubes` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `futbolistas`
 --
 
-INSERT INTO `futbolistas` (`Id_jugador`, `Nombre`, `Apellido`, `Edad`, `Pais`, `Id_equipos`) VALUES
+INSERT INTO `futbolistas` (`Id_Futbolistas`, `Nombre`, `Apellido`, `Edad`, `Pais`, `Id_Clubes`) VALUES
 (1, 'Lionel ', 'Messi', 36, 'Argentina', 1),
 (2, 'Kylian', 'Mbappé', 24, 'Francia', 2),
 (3, 'Erling ', 'Haaland', 23, 'Noruega', 3),
-(4, 'Julián', 'Alvarez', 23, 'Argentina', 3),
-(5, 'Luka', 'Modric', 38, 'Croacia', 4),
+(4, 'Julián  ', 'Alvarez', 23, 'Argentina', 3),
+(5, 'Luka ', 'Modric', 38, 'Croacia', 4),
 (6, 'Thibaut', 'Courtois', 31, 'Belgica', 4),
-(7, 'Robert ', 'Lewandowski', 35, 'Polonia', 6),
+(7, 'Robert', 'Lewandowski', 35, 'Polonia', 6),
 (8, 'Antoine', 'Griezmann', 32, 'Francia', 5),
-(9, 'Kevin ', 'De Bruyne', 32, 'Belgica', 3),
+(9, 'Kevin', 'De Bruyne', 32, 'Belgica', 3),
 (10, 'Jude', 'Bellingham', 20, 'Inglaterra', 4),
-(11, 'Mohamed ', 'Salah', 31, 'Egipto', 7);
+(11, 'Mohamed', 'Salah', 31, 'Egipto', 7);
 
 --
 -- Índices para tablas volcadas
@@ -87,34 +87,24 @@ INSERT INTO `futbolistas` (`Id_jugador`, `Nombre`, `Apellido`, `Edad`, `Pais`, `
 -- Indices de la tabla `clubes`
 --
 ALTER TABLE `clubes`
-  ADD PRIMARY KEY (`Id_equipos`);
+  ADD PRIMARY KEY (`Id_Clubes`);
 
 --
 -- Indices de la tabla `futbolistas`
 --
 ALTER TABLE `futbolistas`
-  ADD PRIMARY KEY (`Id_jugador`),
-  ADD KEY `Id_equipos` (`Id_equipos`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `clubes`
---
-ALTER TABLE `clubes`
-  MODIFY `Id_equipos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  ADD PRIMARY KEY (`Id_Futbolistas`),
+  ADD KEY `Id_Clubes` (`Id_Clubes`);
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `clubes`
+-- Filtros para la tabla `futbolistas`
 --
-ALTER TABLE `clubes`
-  ADD CONSTRAINT `clubes_ibfk_1` FOREIGN KEY (`Id_equipos`) REFERENCES `futbolistas` (`Id_equipos`);
+ALTER TABLE `futbolistas`
+  ADD CONSTRAINT `futbolistas_ibfk_1` FOREIGN KEY (`Id_Clubes`) REFERENCES `clubes` (`Id_Clubes`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
