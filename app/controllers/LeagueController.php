ca4776c3c677dda2e88bb3c3a7a3cc07a8affe3b
ivan <ibatalla13@gmail.com>
2023-10-10 19:38:00 -0300

    add .php

---
+++ app/controllers/LeagueController.php
@@ -0,0 +1,5 @@
+<?php 
+require_once '.app/models/LeagueModel.php';
+require_once '.app/views/LeagueView.php';
+
+
---
+++ prueba.php
@@ -0,0 +1,8 @@
+<?php require_once './templates/header.phtml' ?>
+
+<div>
+    <h1>hola</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe laborum nostrum quam dicta quod corporis aperiam consequuntur? Amet repellendus eligendi laborum rerum cupiditate, id iste voluptate quae eos voluptates cumque.</p>
+</div>
+<?php require_once './templates/footer.phtml' ?>
+
