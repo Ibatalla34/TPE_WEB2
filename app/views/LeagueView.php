6393800e6ebf85a6370c99587a02a1efe3c02efd
Iñaki Diaz <diaz992016@gmail.com>
2023-10-16 20:23:18 -0300

    Se modifico el view

---
+++ app/views/LeagueView.php
@@ -0,0 +1,18 @@
+<?php
+class LeagueView{
+    function renderFutbolistasbyClubes ($clubes, $futbolistas){
+        echo "<table>
+       <thead>
+           <tr>
+               <th>Nombre</th>
+               <th>Apellido</th>
+               <th>Edad</th>
+               <th>Pais</th>
+           </tr>
+       <thead>
+       <tbody>
+       ";
+
+    }
+    
+}
