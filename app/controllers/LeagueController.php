e1a4bf7d8b3c97b5de8a0e80bd5aa27a50fd258e
Iñaki Diaz <diaz992016@gmail.com>
2023-10-16 20:09:02 -0300

    Se modifico el controller

--- app/controllers/LeagueController.php
+++ app/controllers/LeagueController.php
@@ -2,4 +2,22 @@
 require_once '.app/models/LeagueModel.php';
 require_once '.app/views/LeagueView.php';
 
+class LeagueController{
+    private $model;
+    private $view;
+ 
+    public function __construct() {
+        $this->model = new LeagueModel();
+        $this->view = new LeagueView();
+    }
+    function showFutbolistasbyClubes() {
+        if (!isset($_GET['clubes']) || empty($_GET['clubes'])) {
+            $this->view->renderError();
+            return;
+        }
 
+        $clubes = $_GET['clubes'];
+        $futbolistas = $this->model->getFutbolistasbyClubes($clubes);
+        $this->view->renderFutbolistasbyClubes($clubes, $futbolistas);
+    }    
+}
