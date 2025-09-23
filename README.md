177b7ed3dfbb39e8a9569f4b0157bdae58ace0f1
ivan batalla <ibatalla132@gmail.com>
2025-09-23 20:34:30 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,26 @@
+
+#  TPE_WEB2  
+
+## Integrantes
+
+Ivan Batalla(ibatalla132@gmail.com)
+Tomas 
+
+## tematica 
+La tematica sera un catalogo de __futbolistas__
+## descripcion
+
+En el catalogo se incluiran jugadores de futbol, con un nombre,
+el pais al que pertenece, cual es su pie dominante(der,izq,amb),
+la posicion en la cual mas se destaca este mismo,
+su fecha de nacimiento, su altura y a que __equipo pertenece__.
+
+ademas cada equipo tendra un nombre, un pais, una ciudad y su año  de fundacion. sumado a que 
+
+
+
+## diagrama entidad relacion 
+
+![der](DER.png)
+## Sql
+[SQL](futbol.sql)
---
+++ futbol.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-09-2025 a las 01:04:44
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `futbol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos`
+--
+
+CREATE TABLE `equipos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `pais` varchar(30) NOT NULL,
+  `ciudad` varchar(30) NOT NULL,
+  `fundacion` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugadores`
+--
+
+CREATE TABLE `jugadores` (
+  `id` int(11) NOT NULL,
+  `id_equipos` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `pais` varchar(40) NOT NULL,
+  `pierna_buena` varchar(3) NOT NULL,
+  `posicion` varchar(2) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `altura` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_equipos` (`id_equipos`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD CONSTRAINT `jugadores_ibfk_1` FOREIGN KEY (`id_equipos`) REFERENCES `equipos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
