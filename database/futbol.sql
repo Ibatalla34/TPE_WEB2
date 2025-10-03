0542228a90bf71af8d01523acfc2fb5b763b34b5
ivan batalla <ibatalla132@gmail.com>
2025-10-03 16:52:19 -0300

    se modifico la base de datos,se agrego el router y se organizo el codigo

--- database/futbol.sql
+++ database/futbol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-09-2025 a las 01:04:44
+-- Tiempo de generación: 03-10-2025 a las 21:46:35
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -35,6 +35,18 @@ CREATE TABLE `equipos` (
   `fundacion` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `equipos`
+--
+
+INSERT INTO `equipos` (`id`, `nombre`, `pais`, `ciudad`, `fundacion`) VALUES
+(1, 'barcelona', 'españa', 'barcelona', '1999'),
+(2, 'real madrid', 'españa', 'madrid', '1999'),
+(3, 'inter de milan', 'italia', 'milan', '2000'),
+(4, 'juventus', 'italia', 'turin', '2001'),
+(5, 'bayern munich', 'alemania', 'munich', '2000'),
+(6, 'borussia dortmund', 'alemania', 'dortmund', '2002');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +65,24 @@ CREATE TABLE `jugadores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `jugadores`
+--
+
+INSERT INTO `jugadores` (`id`, `id_equipos`, `nombre`, `pais`, `pierna_buena`, `posicion`, `fecha_nacimiento`, `altura`) VALUES
+(1, 1, 'lamine yamal', 'españa', 'izq', 'EI', '2007-07-13', 1.8),
+(2, 1, 'lewandowski', 'polonia', 'der', 'DC', '1998-08-21', 1.85),
+(3, 2, 'Mbappe', 'francia', 'der', 'DC', '1998-12-20', 1.8),
+(4, 3, 'Lautaro Martinez', 'argentina', 'der', 'DC', '1997-08-21', 1.74),
+(5, 2, 'mastantuono', 'argentina', 'izq', 'ED', '2007-08-14', 1.77),
+(6, 3, 'sommer', 'suiza', 'der', 'PO', '1988-12-17', 1.83),
+(7, 4, 'McKennie', 'Estados Unidos', 'der', 'MC', '1998-08-28', 1.85),
+(8, 4, 'conceicao', 'portugal', 'izq', 'ED', '2002-12-14', 1.7),
+(9, 5, 'Neuer', 'alemania', 'der', 'PO', '0000-00-00', 1.93),
+(10, 5, 'Kane', 'inglaterra', 'der', 'DC', '1993-07-28', 1.88),
+(11, 6, 'adeyemi', 'alemania', 'izq', 'ED', '2002-07-01', 1.8),
+(12, 6, 'Sabitzer', 'Austria', 'der', 'MC', '2003-07-24', 1.78);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -68,6 +98,22 @@ ALTER TABLE `equipos`
 ALTER TABLE `jugadores`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_equipos` (`id_equipos`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
