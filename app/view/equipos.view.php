2b02df5cc7a25787134e528f707322a622b03444
Tomas <tomas.vinals@gmail.com>
2025-10-21 17:17:17 -0300

    agrego tempaltes al view de equipos

--- app/view/equipos.view.php
+++ app/view/equipos.view.php
@@ -3,25 +3,11 @@
 class EquipoView{
 
     public function showAll($equipos,$user){
-        require 'templates/layout/header.phtml';
-        echo "<h1>Equipos</h1>";
-        echo "<ul>";
-        foreach($equipos as $equipo){
-            echo "<li><a href='equipo/$equipo->id'>$equipo->nombre</a></li>";
-        }
-        echo "</ul>";
-        require 'templates/layout/footer.phtml';
+      require 'templates/datos.equipo.phtml';
     }
 
-    public function show($jugadores){
-        require 'templates/layout/header.phtml';
-        echo "<h1>Jugadores de ".$jugadores[1]->nombre."</h1>";
-        echo "<ul>";
-        foreach($jugadores[0] as $jugador){
-            echo "<li><a href='jugador/$jugador->id'>$jugador->nombre</a></li>";
-        }
-        echo "</ul>";
-        require 'templates/layout/footer.phtml';
+    public function show($jugadores,$user){
+     require 'templates/lista.jugadores.phtml';
     }
 
 }
